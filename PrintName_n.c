d028cd23ed97bf055ac104628391f7caf27d4582
JINAL2025 <chetanpandya2011@gmail.com>
2025-08-26 17:50:16 +0530

    Add files via upload

---
+++ PrintName_n.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+void main()
+{
+int i, n;
+printf("Enter a positive integer : ");
+scanf("%d", &n);
+printf("Printing my name %d times :\n", n);
+for(i=1;i<=n;i++)
+{
+printf("Jinal\n");
+}
+}
