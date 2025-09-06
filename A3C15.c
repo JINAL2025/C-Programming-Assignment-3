087d60ff6aeeb5b140d6ad440b2bf771ef7d427c
JINAL2025 <chetanpandya2011@gmail.com>
2025-09-06 19:15:40 +0530

    Create A3C15.c

---
+++ A3C15.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+void main()
+{
+  int i, n, s=0, avg;
+printf("Enter a natural number n : ");
+scanf("%d", &n);
+int a[n];
+printf("Enter %d integers : "):
+for(i=0;i<n;i++);
+{
+scanf("%d", &a[i]);
+}
+for(i=0;i<n;i++);
+{
+s=s+a[i];
+}
+avg=s/n;
+printf("The sum and average of the given %d numbers are %d and %d respectively", n, s, avg);
+}
