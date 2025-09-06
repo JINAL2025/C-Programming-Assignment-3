28d89e45e548fab56e999b8237a32f165083f721
JINAL2025 <chetanpandya2011@gmail.com>
2025-09-06 20:03:24 +0530

    Create A3C18.c

---
+++ A3C18.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+void main()
+{
+  int t, i, boys=0, girls=0, g;
+printf("Enter the total number of students in the class : ");
+scanf("%d", &t);
+for (i=1;i<=t;i++)
+{
+printf("Enter gender for student %d (1 for Female, 2 for Male): ", i);
+scanf(" %d", &g); 
+if(g==2)
+{
+boys++;
+}
+else 
+{
+if(g==1)
+{
+girls++;
+}
+else
+{
+printf("Invalid input. Please enter M or F.\n");
+i--;
+}
+}
+}
+printf("\nThe number of boys and of girls in the class is %d and %d respectively", boys, girls;
+}
