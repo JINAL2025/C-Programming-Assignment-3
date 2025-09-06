85f598db5b76e24a635efa33ee88a4d2503333cc
JINAL2025 <chetanpandya2011@gmail.com>
2025-09-06 20:53:15 +0530

    Create A3C24.c

---
+++ A3C24.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+void main()
+{
+int n, r_n=0, k;
+printf("Enter an integer : ");
+scanf("%d", &n);
+while(n>0)
+{
+r=n%10;                                                  //Get the last digit of the number
+r_n = (r_n*10) + r;                                      //Multiply last digit of number by 10 for correct place value and add to reversed number     
+n=n/10;                                                  //Remove the last digit of the number
+}
+printf("The reversed number is %d", r_n);
+}
