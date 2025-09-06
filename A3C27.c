e2e05896e35d00bb1e464afd55f89a97e7256367
JINAL2025 <chetanpandya2011@gmail.com>
2025-09-07 00:04:42 +0530

    Create A3C27.c

---
+++ A3C27.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+void main()
+{
+int n, i;
+printf("Enter a natural number : ");
+scanf("%d", &n);
+printf("The factors of %d are : ", n);
+for(i=1;i<=n;i++)
+{
+if(n%i==0)
+{
+printf("%d  ", i);
+}
+}
+}
