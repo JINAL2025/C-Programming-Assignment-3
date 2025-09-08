329101f6cae0c5b4df669df4ffd0f2661fb5a318
JINAL2025 <chetanpandya2011@gmail.com>
2025-09-08 21:22:27 +0530

    Update A3C25.c

--- A3C25.c
+++ A3C25.c
@@ -1,19 +1,19 @@
 #include <stdio.h>
 void main()
 {
-int n, r_n=0, k;
+int n, k, r_n=0,;
 printf("Enter a positive integer : ");
 scanf("%d", &n);
   //Create the number which is the reverse of the given number
 while(n>0)
 {
-r=n%10;                                                  //Get the last digit of the number
-r_n = (r_n*10) + r;                                      //Multiply last digit of number by 10 for correct place value and add to reversed number     
+k=n%10;                                                  //Get the last digit of the number
+r_n = (r_n*10) + k;                                      //Multiply last digit of number by 10 for correct place value and add to reversed number     
 n=n/10;                                                  //Remove the last digit of the number
 }
 if(r_n==n)
 {
-printf("The number %d" is a palindrome", n);
+printf("The number %d is a palindrome", n);
 }
 else
 {
